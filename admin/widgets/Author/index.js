e5fe54b54de8c75dfdda4c7b45432eea41bd125e
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-01-22 21:49:18 -0200

    :recycle: replace old relation widgets

--- admin/widgets/Author/index.js
+++ admin/widgets/Author/index.js
@@ -1,47 +1,11 @@
-import React, {forwardRef} from 'react'
-import CMS from 'netlify-cms'
-import {renderSuggestion} from './Suggestion'
+import {createCustomRelationWidget} from 'netlify-cms'
+// import {renderSuggestion} from './Suggestion'
 
-const Relation = CMS.getWidget('relation')
+const {control, preview} = CMS.createCustomRelationWidget({
+	collection: 'authors',
+	displayName: 'AuthorControl',
+	searchFields: ['semester', 'title'],
+})
 
-function isValid() {
-	const {value, field} = this.props
-	const required = field.get('required')
-	const error = {
-		type: 'CUSTOM',
-		error: true,
-		message: field.get('error') || 'precisa ser um repórter válido',
-	}
-
-	if (typeof value === 'string' && value !== '') return error
-	if (!required) {
-		if (!value) return true
-		if (!value.slug && !value.name) return true
-	}
-	if (!value) return error
-	if (typeof value.slug !== 'string') return error
-	if (value.slug.length < 1) return error
-	return true
-}
-
-const methods = {
-	isValid,
-	renderSuggestion,
-	shouldRenderSuggestions: () => true,
-	getSuggestionValue: ({slug, data}) => ({
-		title: data.title || '',
-		slug: slug && `/reporteres/${slug}/`,
-	}),
-	valueToFieldString: _value => {
-		const value = _value.toJS ? _value.toJS() : _value
-		return String((value || {}).title || value || '')
-	},
-}
-
-const AuthorControl = forwardRef((props, ref) => (
-	<Relation.control ref={ref} {...props} {...methods} />
-))
-
-AuthorControl.displayName = 'AuthorControl'
-
-export default AuthorControl
+export default control
+export {preview}
--- admin/widgets/Editorial/index.js
+++ admin/widgets/Editorial/index.js
@@ -1,47 +1,12 @@
-import React, {forwardRef} from 'react'
-import CMS from 'netlify-cms'
-import {renderSuggestion} from './Suggestion'
+import {createCustomRelationWidget} from 'netlify-cms'
+// import {renderSuggestion} from './Suggestion'
 
-const Relation = CMS.getWidget('relation')
+const {control, preview} = CMS.createCustomRelationWidget({
+	collection: 'editorials',
+	displayName: 'EditorialControl',
+	searchFields: ['semester', 'title'],
+})
 
-function isValid() {
-	const {value, field} = this.props
-	const required = field.get('required')
-	const error = {
-		type: 'CUSTOM',
-		error: true,
-		message: field.get('error') || 'precisa ser uma editoria válida',
-	}
+export default control
+export {preview}
 
-	if (typeof value === 'string' && value !== '') return error
-	if (!required) {
-		if (!value) return true
-		if (!value.slug && !value.name) return true
-	}
-	if (!value) return error
-	if (typeof value.slug !== 'string') return error
-	if (value.slug.length < 1) return error
-	return true
-}
-
-const methods = {
-	isValid,
-	renderSuggestion,
-	shouldRenderSuggestions: () => true,
-	getSuggestionValue: ({slug, data}) => ({
-		title: data.title || '',
-		slug: slug && `/editorias/${slug}/`,
-	}),
-	valueToFieldString: _value => {
-		const value = _value.toJS ? _value.toJS() : _value
-		return String((value || {}).title || value || '')
-	},
-}
-
-const EditorialControl = forwardRef((props, ref) => (
-	<Relation.control ref={ref} {...props} {...methods} />
-))
-
-EditorialControl.displayName = 'EditorialControl'
-
-export default EditorialControl
