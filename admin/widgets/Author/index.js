c5cd328f058672ddd01e0f2aae04a62a814590e6
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-02-01 18:36:09 -0200

    :mute: remove console.log

--- admin/widgets/Author/index.js
+++ admin/widgets/Author/index.js
@@ -1,8 +1,6 @@
 import {createCustomRelationWidget} from 'netlify-cms'
 // import {renderSuggestion} from './Suggestion'
 
-console.log(createCustomRelationWidget)
-
 const {control, preview} = createCustomRelationWidget({
 	collection: 'authors',
 	displayName: 'AuthorControl',
